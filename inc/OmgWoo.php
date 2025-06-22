a79d40c070da5ddfc6f47fc9c179242aa38ccff1
Dmitry Pripa <dmitrypripa@gmail.com>
2025-06-23 00:53:44 +0300

    add compatibility with dependency manager

--- inc/OmgWoo.php
+++ inc/OmgWoo.php
@@ -1,15 +1,15 @@
 <?php
 namespace OmgWoo;
 
-use OmgCore\Requirement;
+use OmgCore\Dependency;
 
 defined( 'ABSPATH' ) || exit;
 
 class OmgWoo {
 	protected OrderStorage $order_storage;
 
-	public function __construct( string $root_file, Requirement $requirement ) {
-		$requirement->add( 'WooCommerce', 'WooCommerce' );
+	public function __construct( string $root_file, Dependency $dependency ) {
+		$dependency->require_plugin( 'woocommerce', 'WooCommerce', 'woocommerce/woocommerce.php' );
 
 		$this->order_storage = new OrderStorage( $root_file );
 	}
